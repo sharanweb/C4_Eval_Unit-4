c53d15522d7235808e64f83df00ed9692f89a6cb
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-03-28 10:36:46 +0530

    almost ready

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const {register, login}= require("./controllers/auth.controller");
+const todoController = require("./controllers/todo.controller");
+
+app.use("/register", register);
+app.use("/login",login);
+app.use("/todos", todoController);
+
 
 
 
